6a13ae909f2760e31fc50ec51a088e89e0ba1a4c
Larissa Amadeu <104395388+Larissa-Amadeu@users.noreply.github.com>
2023-02-23 13:43:56 -0300

    Update Controlador.php

--- app/Http/Controllers/Controlador.php
+++ app/Http/Controllers/Controlador.php
@@ -1,13 +1,77 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Cadastro;
+use Hash;
+use Session;
 
 class Controlador extends Controller
 {
-    public function teste ()
+    public function inicio ()
     {
-        return view('index');
+        return view('inicio');
     }
+
+    public function cadastro ()
+    {
+      
+        return view('cadastro');
+    }
+    
+    public function login ()
+    {
+        return view('login');
+    }
+    
+    public function formulario ()
+    {
+        return view('formulario');
+    }
+
+    public function cadastroUsuario (Request $request){
+         $request->validate([
+            'name'=> 'required',
+            'email'=>'required|email',
+            'password'=>'required|min:6|max:12',
+            'empresa'=>'required'
+         ]);
+
+         $cadastro = new Cadastro();
+         $cadastro->name=$request->name;
+         $cadastro->email=$request->email;
+         $cadastro->password=Hash::make($request->password);
+         $cadastro->empresa=$request->empresa;
+         $res= $cadastro->save();
+         if($res){
+                return back()->with('succes', 'Usuário registrado com sucesso' );
+         }else{
+                return back()->with('fail','algo deu errado');
+         }
+        }
+
+        
+         public function loginUsuario(Request $request){
+            $request->validate([
+                'email'=>'required|email',
+                'password'=>'required|min:6|max:12',
+             ]);
+
+
+             $cadastro = Cadastro::where('email', '=', $request->email)->first();
+             if($cadastro){
+                if(Hash::ckeck($request->password, $cadastro->password)){
+                    $request->session()->put('loginId', $cadastro->id);
+                    return redirect('dashboard');
+                }else{
+                    return back()->with('fail', 'Senha Incorreta');
+                }
+             }else{
+                return back()->with('fail', 'Email não registrado');
+             }
+         }
+      public function dashboard()
+      {
+        return "Welcome";
+      }
 }
